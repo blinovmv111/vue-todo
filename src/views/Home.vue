8c0e72bb97cb18c78fe1c51e918ff183c10b351d
Mihail Blinov <blinovmv111@gmail.com>
2020-12-17 21:14:30 +0300

    single commit

---
+++ src/views/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h2>Home page</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque placeat, saepe id eum provident
+      iusto? Molestias temporibus totam modi rem.
+    </p>
+    <router-link to="/todos">Todos</router-link>
+  </div>
+</template>
